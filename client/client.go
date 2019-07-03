7f43f419a55d6352cd90e481f651230b6e9c197e
Jax <anyetiangong@qq.com>
2019-07-03 09:36:08 +0800

    连续发送结构化数据的支持

--- client/client.go
+++ client/client.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"time"
 )
 
 func send(conn net.Conn) {
-	words := "Hello World!"
-	conn.Write([]byte(words))
+	for i := 0; i < 30; i++ {
+		dic := make(map[string]interface{})
+		dic["index"] = i
+		dic["timestamp"] = time.Now().Format(time.RFC3339)
+		jsonString, err := json.Marshal(dic)
+		if err != nil {
+			log.Println(err)
+		}
+		length := len(jsonString)
+		if length > 99999 {
+			//Header中标识字符串长度的最大为99999
+			panic("data is too long to send")
+		}
+		lengthText := strconv.Itoa(length)
+		textLength := fmt.Sprintf("%05s", lengthText)[:5]
+		headerText := append([]byte("Header"), textLength...)
+		jsonString = append(headerText, jsonString...)
+		_, err = conn.Write([]byte(jsonString))
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("send : ", string(jsonString))
+	}
 	log.Println("send finished")
 }
 
@@ -15,13 +40,13 @@ func main()  {
 	server := "127.0.0.1:2048"
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", server)
 	if err != nil {
-		log.Println(err.Error())
+		log.Println(err)
 		return
 	}
 
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
-		log.Println(err.Error())
+		log.Println(err)
 		return
 	}
 
--- server/server.go
+++ server/server.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"strconv"
 )
+
+const HeaderText = "Header"
+const HeaderTextLength = len(HeaderText)
+const LengthTextLength = 5
 
 func main() {
 	netListen, err := net.Listen("tcp", "localhost:2048")
 	if err != nil {
-		log.Println(err.Error())
+		log.Println(err)
 		return
 	}
 	defer netListen.Close()
@@ -17,7 +23,7 @@ func main() {
 	for {
 		conn, err := netListen.Accept()
 		if err != nil {
-			log.Println(err.Error())
+			log.Println(err)
 			continue
 		}
 		log.Println(conn.RemoteAddr().String(), " tcp connect success")
@@ -26,13 +32,34 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-	buffer := make([]byte, 2048)
+	buffer := make([]byte, 2048, 2048)
 	for {
 		n, err := conn.Read(buffer)
 		if err != nil {
-			log.Println(conn.RemoteAddr().String(), " read error: ", err.Error())
+			log.Println(conn.RemoteAddr().String(), " read error: ", err)
 			return
 		}
-		log.Println(conn.RemoteAddr().String(), "receive data string:\n", string(buffer[:n]))
+		tmpBuffer := buffer[:n]
+		log.Println(conn.RemoteAddr().String(), "receive data string:")
+		//解析buffer中的内容
+		for {
+			if len(tmpBuffer) == 0 {
+				break
+			}
+
+			if string(tmpBuffer[:HeaderTextLength]) != HeaderText {
+				panic("buffer not started with 'Header'")
+			}
+			lengthText := string(tmpBuffer[HeaderTextLength: HeaderTextLength + LengthTextLength])
+			textLength, err := strconv.Atoi(lengthText)
+			if err != nil {
+				log.Println(err)
+			}
+			content := tmpBuffer[HeaderTextLength + LengthTextLength: HeaderTextLength + LengthTextLength + textLength]
+			tmpBuffer = tmpBuffer[HeaderTextLength + LengthTextLength + textLength:]
+
+			fmt.Println(string(content))
+		}
+
 	}
 }
